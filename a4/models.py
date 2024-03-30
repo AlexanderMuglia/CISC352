8cd0a367285d499cdce5bd25983d77f3cc946b80
alex <alexander.muglia@hotmail.com>
2024-03-30 16:07:27 -0400

    q3

--- a4/models.py
+++ a4/models.py
@@ -141,7 +141,18 @@ class DigitClassificationModel(object):
 
     def __init__(self):
         # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
+        # why negative???
+        self.learning_rate = -0.050  # minimum allowed (0.001, 1.0)
+        self.batch_size = 50
+
+        # first layer starts with 1
+        # last layer ends with 1
+        self.w1 = nn.Parameter(784, 20)  # (input size, output size)
+        self.b1 = nn.Parameter(1, 20)  # (1, same as weight)
+        self.w2 = nn.Parameter(20, 100)  # (input size, output size)
+        self.b2 = nn.Parameter(1, 100)   # (1, same as weight)
+        self.w3 = nn.Parameter(100, 10)  # (input size, output size)
+        self.b3 = nn.Parameter(1, 10)   # (1, same as weight)
 
     def run(self, x):
         """
@@ -157,7 +168,10 @@ class DigitClassificationModel(object):
             A node with shape (batch_size x 10) containing predicted scores
                 (also called logits)
         """
-        "*** YOUR CODE HERE ***"
+        l1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        l2 = nn.ReLU(nn.AddBias(nn.Linear(l1, self.w2), self.b2))
+        l3 = nn.AddBias(nn.Linear(l2, self.w3), self.b3)
+        return l3
 
     def get_loss(self, x, y):
         """
@@ -172,10 +186,27 @@ class DigitClassificationModel(object):
             y: a node with shape (batch_size x 10)
         Returns: a loss node
         """
-        "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(x), y)
 
     def train_model(self, dataset):
         """
         Trains the model.
         """
-        "*** YOUR CODE HERE ***"
+        val_acc = 0
+        for x, y in dataset.iterate_forever(self.batch_size):
+            val_acc = dataset.get_validation_accuracy()
+            if val_acc >= 0.98:
+                break
+            # Calculates loss
+            loss = self.get_loss(x, y)
+            cur_loss = nn.as_scalar(loss)
+
+            # Updates weights from gradients
+            [w1_grad, b1_grad, w2_grad, b2_grad, w3_grad, b3_grad] = nn.gradients(
+                [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3], loss)
+            self.w1.update(self.learning_rate, w1_grad)
+            self.b1.update(self.learning_rate, b1_grad)
+            self.w2.update(self.learning_rate, w2_grad)
+            self.b2.update(self.learning_rate, b2_grad)
+            self.w3.update(self.learning_rate, w3_grad)
+            self.b3.update(self.learning_rate, b3_grad)
